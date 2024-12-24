62a40d841eb37c5267318f903cd8d541eb6240e5
TemporaryGITHUB <166900887+Egoistsms@users.noreply.github.com>
2024-12-24 09:50:16 +0700

    Update Afk 30min.lua

--- Afk 30min.lua
+++ Afk 30min.lua
@@ -1,20 +1,24 @@
-setfpscap(15)
+setfpscap(5)
+
 local afkTime = 30 * 60
+local lastMove = tick()
+
+game.Players.LocalPlayer.Idled:Connect(function()
+    game:GetService('VirtualUser'):ClickButton2(Vector2.new())
+    lastMove = tick()
+end)
 
 game.Players.CharacterAdded:Connect(function(character)
-  local player = character.Parent
-  local lastMove = tick()
+    local player = game.Players.LocalPlayer
+    character:WaitForChild("Humanoid").Running:Connect(function()
+        lastMove = tick()
+    end)
 
-  character.LastMovementTime.Changed:Connect(function()
-    lastMove = tick()
-  end)
-
-  player:Chat("Execute!")
-
-  while wait(1) do
-    if tick() - lastMove >= afkTime then
-      player:Kick("Execute Breakable!")
-      break
+    while true do
+        wait(1)
+        if tick() - lastMove >= afkTime then
+            player:Kick("This account has been online for 30 minutes")
+            break
+        end
     end
-  end
 end)
